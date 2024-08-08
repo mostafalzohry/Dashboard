6140a067ab6a395bf9dcd4c30ced34bc631cfee3
Mostafa Elzohry <98609624+mostafalzohry@users.noreply.github.com>
2024-08-08 18:19:35 +0300

    delete old config firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -5,13 +5,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics"; 
 
 const firebaseConfig = {
-  // apiKey: "AIzaSyDUbqVgB7XxmLf-d_zC95Ppci-kiuktYTY",
-  // authDomain: "users-15823.firebaseapp.com",
-  // projectId: "users-15823",
-  // storageBucket: "users-15823.appspot.com",
-  // messagingSenderId: "966911957408",
-  // appId: "1:966911957408:web:dbfbfe41d5f8dfb555ecc5",
-  // measurementId: "G-VCP53SJX5R"
+
   apiKey: "AIzaSyAXrDFzNDXR-LX5pZO05hdVEpMX7cQ2Fts",
   authDomain: "dashboard-8ece8.firebaseapp.com",
   projectId: "dashboard-8ece8",
