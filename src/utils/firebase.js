57e6fb1fc5674010a1617f85ff17c6eaddbfd8fa
Mostafa Elzohry <98609624+mostafalzohry@users.noreply.github.com>
2024-08-08 18:40:19 +0300

    delete comments

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,8 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth  } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics"; 
 
 const firebaseConfig = {
 
@@ -14,7 +12,6 @@ const firebaseConfig = {
   appId: "1:218375136658:web:ab498272c7443282458d4b"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
